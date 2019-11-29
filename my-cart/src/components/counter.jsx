373f2ec4907a6bda1ad08a95758d38e8bfeafd4d
Ayesha Liaquat <ayesha.liaquat@jintech.com>
2019-11-29 10:23:24 +0500

    c

--- my-cart/src/components/counter.jsx
+++ my-cart/src/components/counter.jsx
@@ -4,13 +4,22 @@ class Counter extends Component {
     count: 0,
     address: {
       street: ""
-    }
+    },
+    tags: ["tag1", "tag2"]
   };
   render() {
+    let classes = "badge badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={classes}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        {/* How to loop items */}
+        <ul>
+          {this.state.tags.map(tag => (
+            <li>{tag}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
   }
@@ -21,3 +30,7 @@ class Counter extends Component {
 }
 
 export default Counter;
+
+// short cuts
+// cc == generate basic class syntax
+//imrc == import classed
